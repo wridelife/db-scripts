3371ecb88942a4c85665ad3f7d01e53ad145391b
wridelife <158450162+wridelife@users.noreply.github.com>
2024-03-04 17:43:23 +0530

    Add files via upload
    DB Scripts First Commit

---
+++ DB-Scripts.sql
@@ -0,0 +1,10 @@
+
+--------------------DB-SCRIPTS(OTP_LOGIN)---------------------------
+ALTER TABLE ride_request CHANGE minimum_fare night_fare double;
+ALTER TABLE services CHANGE minimum_fare night_fare double;
+ALTER TABLE users ADD isRiderRegistered boolean;
+---------------------new changes-----------------------------------------
+alter table users  modify column email  varchar(250) Null;
+alter table users  modify column username  varchar(250) Null;
+alter table users  modify column password  varchar(250) Null;
+-----------------------------------------------------------------------------
