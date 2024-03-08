ae111504feeeac2972d2f3eb03090e15fb4765e2
wridelife <158450162+wridelife@users.noreply.github.com>
2024-03-09 03:09:57 +0530

    Update DB-Scripts.sql (OTP TABLE)

--- DB-Scripts.sql
+++ DB-Scripts.sql
@@ -1,4 +1,5 @@
-
+------------------------OTP TABLE----------------------
+create table if not exists user_otps ( id bigint unsigned auto_increment primary key, user_id bigint unsigned not null, otp varchar(255) not null, created_at timestamp null, updated_at timestamp null );
 --------------------DB-SCRIPTS(OTP_LOGIN)---------------------------
 ALTER TABLE ride_requests CHANGE minimum_fare night_fare double;
 ALTER TABLE services CHANGE minimum_fare night_fare double;
